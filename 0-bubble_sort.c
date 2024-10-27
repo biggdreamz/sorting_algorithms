7cfd458cdae66a60b2bd56945f65e89a4e221819
ABUBAKAR ABDUL <abdulabubakarsadiq94@gmail.com>
2024-10-28 00:21:17 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,31 @@
 #include "sort.h"
 /**
- * bubble_sort - sort array lements from min to max value
+ * bubble_sort - sort array elements from min to max value
  * @array: array
  * @size: array size
  */
 void bubble_sort(int *array, size_t size)
 {
-
-	size_t i, index, tmp = 0;
+	size_t i, index;
+	int tmp;
 
 	if (size < 2)
 		return;
-	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (index = 0; index < size - i - 1; index++)
 		{
-			if (array[index] > array[index + 1] && array[index + 1])
+			if (array[index] > array[index + 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				// Swap elements
+				tmp = array[index];
+				array[index] = array[index + 1];
+				array[index + 1] = tmp;
+
+				// Print array after each swap
+				print_array(array, size);
 			}
 		}
+	}
 }
