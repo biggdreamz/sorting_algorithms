3c07b673e753a0886e3f49e4279d098187e678cf
ABUBAKAR ABDUL <abdulabubakarsadiq94@gmail.com>
2024-10-28 16:58:48 +0100

    Update 103-merge_sort.c

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -94,24 +94,3 @@ void merge_sort(int *array, size_t size)
 
 	free(temp);
 }
-
-/**
- * print_array - Prints an array of integers
- * @array: Pointer to the array to be printed
- * @size: Size of the array
- *
- * Description: This function prints each element of an integer array
- * separated by commas.
- */
-void print_array(const int *array, size_t size)
-{
-	size_t i;
-
-	for (i = 0; i < size; i++)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", array[i]);
-	}
-	printf("\n");
-}
