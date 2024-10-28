452ec3c8055876e975204b1e32e050623a00a239
ABUBAKAR ABDUL <abdulabubakarsadiq94@gmail.com>
2024-10-28 01:41:16 +0100

    sort update

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,60 +1,61 @@
+#include <stdlib.h>
+#include <string.h>
 #include "sort.h"
-#include <stdio.h>
-/**
- *_calloc - this is a calloc function
- *@nmemb: number of elemets
- *@size: bit size of each element
- *Return: pointer to memory assignement
- */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	unsigned int i = 0;
-	char *p;
 
-	if (nmemb == 0 || size == 0)
-		return ('\0');
-	p = malloc(nmemb * size);
-	if (p == '\0')
-		return ('\0');
-	for (i = 0; i < (nmemb * size); i++)
-		p[i] = '\0';
-	return (p);
-}
 /**
- * counting_sort - this is a counting sort method implementation
- * @array: array to sort
- * @size: array size
+ * counting_sort - Implements counting sort algorithm.
+ * @array: Array to sort.
+ * @size: Array size.
  */
 void counting_sort(int *array, size_t size)
 {
-	int index, maximun = 0, *counter = '\0', *tmp = '\0';
+	int index, maximum = 0;
+	int *counter = NULL;
+	int *tmp = NULL;
 	size_t i;
 
-	if (array == '\0' || size < 2)
+	if (array == NULL || size < 2)
 		return;
-	/* find maximun number */
+
+	/* Find maximum number */
 	for (i = 0; i < size; i++)
-		if (array[i] > maximun)
-			maximun = array[i];
-	counter = _calloc(maximun + 1, sizeof(int));
-	tmp = _calloc(size + 1, sizeof(int));
-	/* count the array elements */
+	{
+		if (array[i] > maximum)
+			maximum = array[i];
+	}
+
+	/* Allocate memory for counter and tmp */
+	counter = calloc(maximum + 1, sizeof(int));
+	tmp = calloc(size, sizeof(int));
+
+	if (!counter || !tmp)
+	{
+		free(counter);
+		free(tmp);
+		return;
+	}
+
+	/* Count the array elements */
 	for (i = 0; i < size; i++)
 		counter[array[i]]++;
-	/* get the accumulative values */
-	for (index = 1; index <= maximun; index++)
+
+	/* Get the accumulative values */
+	for (index = 1; index <= maximum; index++)
 		counter[index] += counter[index - 1];
-	print_array(counter, maximun + 1);
-	/* get the new array sorted */
+
+	print_array(counter, maximum + 1); /* Assuming this prints the array */
+
+	/* Get the new sorted array */
 	for (i = 0; i < size; ++i)
 	{
 		tmp[counter[array[i]] - 1] = array[i];
 		counter[array[i]]--;
 	}
-	/* replace old array to new array sorted */
+
+	/* Replace old array with new sorted array */
 	for (i = 0; i < size; i++)
 		array[i] = tmp[i];
+
 	free(tmp);
 	free(counter);
-
 }
